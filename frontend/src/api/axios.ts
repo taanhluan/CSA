602111dce47b188efa3260cc8b48973a4176b4b5
Jonathan Ta <117909864+taanhluan@users.noreply.github.com>
2025-06-16 16:50:59 +0000

    🔧 add axios env

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 export default api;
