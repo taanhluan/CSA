9e0330bb405bf69f4d353f6a63017f9f07156dc3
Jonathan Ta <117909864+taanhluan@users.noreply.github.com>
2025-06-16 17:04:35 +0000

    🔧 add axios env

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// Base URL cho backend
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000", // KHÔNG có /api
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
